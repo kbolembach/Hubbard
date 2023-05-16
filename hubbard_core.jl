aeefe64bae4cd43a50aaaee88de1b77916a5df73
kbolembach <konrad.bolembach@gmail.com>
2023-05-16 20:27:07 +0200

    reworked mixed svd-lanczos diagonalization

--- hubbard_core.jl
+++ hubbard_core.jl
@@ -1,98 +1,30 @@
+__precompile__()
+
+using SparseArrays
+using Combinatorics
+
 struct Params
     dim_sizes # tuple with sizes of each dimension
     next
 end
 
-""" 
-    create(state_vector, pos)
-
-Acts with a creation operator upon a state.
-
-
-### Input
-
- - `state_vector` -- a vector with information on the state in the bit representation, i.e. each element is either 1 or 0 depending if there is a boson in the given position. 
- - `pos` -- an integer representing the position; for models including spin, odd positions (2n - 1) are spin up, even positions (2n) are spin down, n being the lattice point index.
- 
-### Output
-
-A modified copy of the state vector, or scalar 0 if the target position was already occupied or the state vector was an integer (for redundancy reasons).
-
-### Examples
-
-```julia-repl
-julia> create([0, 0, 1, 0], 2)
-4-element Vector{Int64}:
- 0
- 1
- 1
- 0
- ```"""
-function create(state_vector, pos)
-    if typeof(state_vector) == Int
-        return 0
-    end
-    if state_vector[pos] == 1
-        return 0
-    end
-    out = copy(state_vector)
-    out[pos] = 1
-    
-    out
-end
-
-
-""" 
-    annihilate(state_vector, pos)
-
-Acts with an annihilation operator upon a state.
-
-
-### Input
-
- - `state_vector` -- a vector with information on the state in the bit representation, i.e. each element is either 1 or 0 depending if there is a boson in the given position. 
- - `pos` -- an integer representing the position; for models including spin, odd positions (2n - 1) are spin up, even positions (2n) are spin down, n being the lattice point index.
- 
-### Output
-
-A modified copy of the state vector, or scalar 0 if the target position was already occupied or the state vector was an integer (for redundancy reasons).
-
-### Examples
-
-```julia-repl
-julia> annihilate([0, 1, 1, 0], 3)
-4-element Vector{Int64}:
- 0
- 1
- 0
- 0
- ```"""
-function annihilate(state_vector, pos)
-    if typeof(state_vector) == Int
-        return 0
-    end
-    if state_vector[pos] == 0
-        return 0
-    end
-    out = copy(state_vector)
-    out[pos] = 0
-    
-    out
-end
 
 function move_particle(state_vector, spin, original_pos, target_pos)
     if typeof(state_vector) == Int
-        return 0
+        return -1
     end
     if state_vector[original_pos...][spin] == 0 || state_vector[target_pos...][spin] == 1
-        return 0
+        return -1
     end
 
-    out = deepcopy(state_vector)
-    out[original_pos...][spin]  = 0
-    out[target_pos...][spin]  = 1
+    state_vector[original_pos...][spin]  = 0
+    state_vector[target_pos...][spin]  = 1
+    idx = flatstate_to_idx(state_vector)
+    # undo changes:
+    state_vector[original_pos...][spin]  = 1
+    state_vector[target_pos...][spin]  = 0
 
-    return out
+    return idx
 end
 
 # get the tuples of coordinates for each neighbor of idx
@@ -133,15 +65,23 @@ function idx_to_fullstate(idx, dim_sizes)
 end
 
 # Converts bit state vector to the index it represents (e.g. [1, 1, 0, 0] -> 4)
-function state_to_idx(state) 
+function flatstate_to_idx(state) 
     state = collect(Iterators.flatten(state))
     reduce(+, state .* [2^n for n in 0:length(state)-1]) + 1
+end
+
+function fullstate_to_idx(fullstate)
+    state = collect(Iterators.flatten(fullstate)) 
+    flatstate_to_idx(state)
 end
 
 # Converts bit state vector to the full state vector (e.g. [1, 1, 0, 0] becomes a vector of length 16)
 function state_to_statevec(state) 
     state = collect(Iterators.flatten(state))   
-    [x == state_to_idx(state) ? 1.0 : 0.0 for x in 1:2^(length(state))]
+    # [x == state_to_idx(state) ? 1.0 : 0.0 for x in 1:2^(length(state))] 
+    out = zeros(2^(length(state)))
+    out[flatstate_to_idx(state)] = 1
+    return out
 end
 
 function flatstate_to_fullstate(flatstate, dim_sizes)
@@ -149,32 +89,69 @@ function flatstate_to_fullstate(flatstate, dim_sizes)
     reshape(halfstate, dim_sizes)
 end
 
-# perform H |state> and get statevec of: alpha |state'>
+# perform H |state>
 function hamiltonian_on_state(params::Params, U, t, chem_pot, state)
     # total = reshape([[0,0] for _ in 1:reduce(*, params.dim_sizes)], params.dim_sizes)
     N = reduce(*, params.dim_sizes)
-    total = zeros(2^(2N))
+    total = sparsevec([], [], 4^N)
+    
+    stvecidx = fullstate_to_idx(state)
+    # (interacting - chem) term
+    total[stvecidx] = U * reduce(+, [pos[1]*pos[2] for pos in state]) - chem_pot * reduce(+, collect(Iterators.flatten(state)))
 
-    # nonzero_entries_indices = [] # przepisać kod, by skipował zera
+    # kinetic term
     for idx in CartesianIndices(state)
         for neighbor in forward_neighbors(idx, params.next)
-            tmp1 = move_particle(state, 1, neighbor, Tuple(idx))
-            if typeof(tmp1) != Int
-                total[state_to_idx(tmp1)] += 1
+            idx1 = move_particle(state, 1, neighbor, Tuple(idx))
+            if idx1 != -1
+                total[idx1] = t
             end
 
-            tmp2 = move_particle(state, 2, neighbor, Tuple(idx))
-            if typeof(tmp2) != Int
-                total[state_to_idx(tmp2)] += 1
+            idx2 = move_particle(state, 2, neighbor, Tuple(idx))
+            if idx2 != -1
+                total[idx2] = t
             end
         end
     end
 
-    kinetic = t * total
-    interacting = U * reduce(+, [pos[1]*pos[2] for pos in state]) .* state_to_statevec(state)
-    chemical = chem_pot * reduce(+, collect(Iterators.flatten(state))) .* state_to_statevec(state)
+    return total
+end
+
+
+function hamiltonian_on_state(params::Params, U, t, chem_pot, 
+    fullstate, no_electrons, inv_ids)
+
+    # total = reshape([[0,0] for _ in 1:reduce(*, params.dim_sizes)], params.dim_sizes)
+    N = reduce(*, params.dim_sizes)
+    len_flatstates = binomial(2N, no_electrons)
+    # base_flatstate = [ones(Int, no_electrons)..., zeros(Int, 2N - no_electrons)...]
+    # flatstates = multiset_permutations(base_flatstate, 2N)
+    # indices = flatstate_to_idx.(flatstates)
     
-    interacting + kinetic - chemical
+    total = sparsevec([], [], len_flatstates)
+
+    # function fullidx_to_limitedidx(full_idx, indices)
+    #     findfirst(isequal(full_idx), indices)
+    # end
+    stvecidx = inv_ids[fullstate_to_idx(fullstate)]
+    # stvecidx = findfirst(isequal(flatstate), flatstates)
+
+    # (interacting - chem) term
+    total[stvecidx] = U * reduce(+, [pos[1]*pos[2] for pos in fullstate]) - chem_pot * reduce(+, collect(Iterators.flatten(fullstate)))
+
+    # kinetic term
+    for idx in CartesianIndices(fullstate)
+        for neighbor in forward_neighbors(idx, params.next)
+            for spin in [1, 2]
+                idx1 = move_particle(fullstate, spin, neighbor, Tuple(idx))
+                if idx1 != -1
+                    total[inv_ids[idx1]] = t
+                end
+            end
+        end
+    end
+
+    return total
 end
 
 
@@ -184,21 +161,157 @@ end
 
 
 # for every H |state> multiply it by every possible <state'|
-function get_hamiltonian(params::Params, U, t, chem_pot)
-    N = reduce(*, params.dim_sizes)
-    hamiltonian = zeros(4N^2, 4N^2)
+function get_hamiltonian(hamiltonian::SparseMatrixCSC, params::Params, 
+    U, t, chem_pot)
 
-    right_side = [hamiltonian_on_state(params, U, t, chem_pot, idx_to_fullstate(idx, params.dim_sizes)) for idx in 0:(4^N - 1)]
-    for i in 1:4N^2, j in 1:4N^2
-        left_state = [i == idx ? 1 : 0 for idx in 1:4^N]
-        hamiltonian[i, j] = braket(left_state, right_side[j])
+    N = reduce(*, params.dim_sizes)
+    # hamiltonian = zeros(4^N, 4^N)
+    # hamiltonian = sprand(4^N, 4^N, 16.0^-N) ./ 10^6
+
+    fn(idx) = hamiltonian_on_state(params, U, t, chem_pot, idx_to_fullstate(idx, params.dim_sizes)) # for idx in 0:(4^N - 1)
+    for j in 1:4^N
+        # left_state = [i == idx ? 1 : 0 for idx in 1:4^N]
+        # hamiltonian[i, j] = braket(left_state, right_side[j])
+        idxs, vals = findnz(fn(j-1))
+        for (i, val) in zip(idxs, vals)
+            if val != 0
+                hamiltonian[i, j] = val
+                if i != j
+                    hamiltonian[j, i] = conj(val)
+                end
+            end
+        end
     end
     
-    hamiltonian
+    return hamiltonian
+end
+
+# get the hamiltonian for a set number of electrons
+function get_hamiltonian(params::Params, U, t, chem_pot, no_e, sparse, ids, inv_ids)
+
+    N = reduce(*, params.dim_sizes)
+    len_flatstates = binomial(2N, no_e)
+    
+    if sparse
+        hamiltonian = sprand(len_flatstates, len_flatstates, len_flatstates^(-2.0)) ./ 10^6
+    else
+        hamiltonian = zeros(len_flatstates, len_flatstates)
+    end
+
+    for (j, id) in enumerate(ids) 
+        idxs, vals = findnz(hamiltonian_on_state(params, U, t, chem_pot, idx_to_fullstate(id-1, params.dim_sizes), no_e, inv_ids))
+        for (i, val) in zip(idxs, vals)
+            if val != 0
+                hamiltonian[i, j] = val
+                if i != j
+                    hamiltonian[j, i] = conj(val)
+                end
+            end
+        end
+    end
+    
+    return hamiltonian
 end
 
 
-function no_particles_from_state(state, N)
-    multiplier = [reduce(+, digits(n, base=2)) for n in 0:(4N^2 -1)]
+function get_hamiltonian(hamiltonian::Matrix, params::Params, U, t, chem_pot)
+
+    N = reduce(*, params.dim_sizes)
+
+    fn(idx) = hamiltonian_on_state(params, U, t, chem_pot, idx_to_fullstate(idx, params.dim_sizes)) # for idx in 0:(4^N - 1)
+    for j in 1:4^N
+        idxs, vals = findnz(fn(j-1))
+        for (i, val) in zip(idxs, vals)
+            if val != 0
+                hamiltonian[i, j] = val
+                if i != j
+                    hamiltonian[j, i] = conj(val)
+                end
+            end
+        end
+    end
+    
+    return hamiltonian
+end
+
+
+function no_particles_from_state(state, multiplier)
+    # include this multiplier in general code for better performance:
+    # multiplier = [reduce(+, digits(n, base=2)) for n in 0:(4^N -1)]
     braket(state, multiplier.*state)
 end
+
+# z symetrią liczby cząstek
+function compute_thermodynamic_quantities(params, chem_pot, temperature, U, t, avg_no_multiplier)
+    
+    N = reduce(*, params.dim_sizes)
+    energies = []
+    H_states = []
+    avg_particles = []
+    for no_e in 0:2N
+        base_flatstate = [ones(Int, no_e)..., zeros(Int, 2N - no_e)...]
+        flatstates = multiset_permutations(base_flatstate, 2N)
+        no_possible_states = binomial(2N, no_e)
+        ids = [flatstate_to_idx(state) for state in flatstates]
+        inv_ids = Dict(id => i for (id, i) in zip(ids, 1:no_possible_states))
+
+        if no_possible_states > 100
+            H_matrix = get_hamiltonian(params, U, t, chem_pot, no_e, true, ids, inv_ids)
+            out = Array{Float64}(undef, no_possible_states)
+            fill!(out, NaN)
+            t_energies, t_H_states, conv_info = out, out, out
+            try
+                t_energies, t_H_states, conv_info = eigsolve(H_matrix, 4, :SR, tol=1e-27)
+                append!(energies, t_energies...)
+                append!(H_states, t_H_states...)
+                append!(avg_particles, [real(dot(state, no_e.*state)) for state in t_H_states]...)
+            catch e
+                println("Error $e at μ=$chem_pot, T=$temperature, U=$U, t=$t")
+                return out, NaN, NaN, NaN
+            end
+        else
+            # hamiltonian = zeros(no_possible_states, no_possible_states)
+            H_matrix = get_hamiltonian(params, U, t, chem_pot, no_e, false, ids, inv_ids)
+            diagonalized = eigen(H_matrix)
+            t_energies = diagonalized.values
+            t_H_states = Vector{Float64}[eachcol(diagonalized.vectors)...]
+            append!(energies, t_energies...)
+            append!(H_states, t_H_states...)
+            append!(avg_particles, [real(dot(t_state, no_e.*t_state)) for t_state in t_H_states]...)
+        end
+    end
+    exp_vec = exp.(-real(energies) / (k*temperature))
+    stat_sum = reduce(+, exp_vec)
+    # avg_particles = [no_particles_from_state(H_state, avg_no_multiplier) for H_state in H_states]
+    
+    densities = real(exp_vec ./ stat_sum)
+    avg_particles_temp = reduce(+, densities .* avg_particles)
+    entropy = -k * reduce(+, [abs(p)*log(abs(p)) for p in densities])
+    grand_potential = -k*temperature*log(stat_sum)
+    return densities, avg_particles_temp, entropy, grand_potential
+end
+
+# tylko lanczos
+# function compute_thermodynamic_quantities(params, chem_pot, temperature, U, t, avg_no_multiplier)
+#     N = reduce(*, params.dim_sizes)
+#     hamiltonian = sprand(4^N, 4^N, 16.0^-N) ./ 10^6
+#     H_matrix = get_hamiltonian(hamiltonian, params, U, t, chem_pot)
+#     out = Array{Float64}(undef, 4N^2)
+#     fill!(out, NaN)
+#     energies, H_states, conv_info = out, out, out
+#     try
+#         energies, H_states, conv_info = eigsolve(H_matrix, 4, :SR, tol=1e-27)
+#     catch e
+#         print("Error $e at μ=$chem_pot, T=$temperature, U=$U, t=$t")
+#         return out, out, out, out
+#     end
+#     exp_vec = exp.(-real(energies) / (k*temperature))
+#     stat_sum = reduce(+, exp_vec)
+#     avg_particles = [no_particles_from_state(H_state, avg_no_multiplier) for H_state in H_states]
+    
+#     densities = exp_vec ./ stat_sum
+#     avg_particles_temp = reduce(+, densities .* avg_particles)
+#     entropy = -k * reduce(+, [abs(p)*log(abs(p)) for p in densities])
+#     grand_potential = -k*temperature*log(stat_sum)
+#     return densities, avg_particles_temp, entropy, grand_potential
+# end
--- hubbard_lanczos_script.jl
+++ hubbard_lanczos_script.jl
@@ -3,6 +3,8 @@ using PlotlyJS
 using KrylovKit
 using ArgParse
 using Dates
+using InteractiveUtils
+using DelimitedFiles
 
 include("hubbard_core.jl")
 
@@ -10,6 +12,14 @@ include("hubbard_core.jl")
 function parse_commandline()
     s = ArgParseSettings()
     @add_arg_table s begin
+        "--U"
+            help = "strength of the coulomb interaction"
+            default = 1.0
+            arg_type = Float64
+        "--t"
+            help = "hopping integral"
+            default = 1.0
+            arg_type = Float64
         "--chem_max"
             help = "maximum and minimum chemical potential in range"
             default = 5.0
@@ -21,88 +31,90 @@ function parse_commandline()
         "--lattice_dims"
             help = "specify lattice sizes as d1,d2,...,dn"
             default = "2"
+        "--temperatures"
+            help = "specify lattice sizes as t1,t2,...,tn"
+            default = "1.0"
     end
     return parse_args(s)
 end
 
 
 """Helper function to draw graphs of data and save them with a given name."""
-function save_graph(chem_potentials, y_vals, U, t, temp_range, name)
+function save_graph(path, chem_potentials, y_vals, U, t, temp_range, name)
+    str_temp_range = string(temp_range)
     layout = Layout(
-    title="$name od μ, U=$U, t=$t",
-    xaxis_title="μ",
-    yaxis_title="",
+        title="$name od μ, U=$U, t=$t; temperatury $str_temp_range",
+        xaxis_title="μ",
+        yaxis_title="",
     )
     pl = plot([scatter(x=chem_potentials, y=yval, name="T=$t") for (yval, t) in zip(y_vals, temp_range)], layout)
-    savefig(pl, "$name plot.png")
-    savefig(pl, "$name plot.html")
-end
-
-
-function compute_thermodynamic_quantities(params, chem_pot, temperature, U, t)
-    k = 1.0
-    N = reduce(*, params.dim_sizes)
-    H_matrix = get_hamiltonian(params, U, t, chem_pot)
-    out = Array{Float64}(undef, 4N^2)
-    fill!(out, NaN)
-    energies, H_states, conv_info = out, out, out
-    try
-        energies, H_states, conv_info = eigsolve(H_matrix, 4, :SR, tol=1e-20)
-    catch e
-        print("Error $e at μ=$chem_pot, T=$temperature, U=$U, t=$t")
-        return out, out, out, out
+    savefig(pl, path * "$name plot.png")
+    savefig(pl, path * "$name plot.html")
+    for (temp, data) in zip(temp_range, y_vals)
+        writedlm(path * "$name, temp=$temp.csv", data)
     end
-    exp_vec = exp.(-real(energies) / (k*temperature))
-    stat_sum = reduce(+, exp_vec)
-    avg_particles = [no_particles_from_state(H_state, N) for H_state in H_states]
-    
-    densities = exp_vec ./ stat_sum
-    avg_particles_temp = reduce(+, densities .* avg_particles)
-    entropy = -k * reduce(+, [abs(p)*log(abs(p)) for p in densities])
-    grand_potential = -k*temperature*log(stat_sum)
-    return densities, avg_particles_temp, entropy, grand_potential
 end
 
+
+k = 1.0
 
 function main()
+    t_start_program = now()
+    n_threads = Threads.nthreads()
     nw = Dates.format(now(), "mm-dd HH:MM:SS")
-    println("$nw  Begin program.")
+    println("$nw  Begin program with $n_threads threads.")
 
     parsed_args = parse_commandline()
-    nw = Dates.format(now(), "mm-dd HH:MM:SS")
-    println("$nw  Parsed args.")
-
     CHEM_MAX = parsed_args["chem_max"]
     CHEM_STEP = parsed_args["chem_step"]
     STR_LATTICE_DIMS = parsed_args["lattice_dims"]
     LATTICE_DIMS = Tuple(parse.(Int64, split(STR_LATTICE_DIMS, ",")))
-
     N = reduce(*, LATTICE_DIMS)
-    all_states = reshape(Iterators.product([[0,1] for _ in 1:2N]...) .|> collect, (1, 4^N))
-    next(n) = [collect(2:n); 1]
-    params = Params(LATTICE_DIMS, 
-        all_states,
-        [flatstate_to_fullstate(state, LATTICE_DIMS) for state in all_states],
-        [next(d) for d in LATTICE_DIMS])
+    U = parsed_args["U"]
+    t = parsed_args["t"]
+    STR_TEMPERATURES = parsed_args["temperatures"]
+    temp_range = Tuple(parse.(Float64, split(STR_TEMPERATURES, ",")))
+    
+    nw = Dates.format(now(), "mm-dd HH:MM:SS")
+    println("$nw  Parsed args.")
+
+    nw = Dates.format(now(), "mm-dd HH-MM-SS")
+    path = "./hubbard_results/$STR_LATTICE_DIMS T=$nw/"
+    mkpath(path)
 
     chem_potentials = range(-CHEM_MAX, stop=CHEM_MAX, step=CHEM_STEP)
-    temp_range = round.(10.0.^(range(-0.1,stop=1.0,length=3)), digits=3)
+    multiplier = [reduce(+, digits(n, base=2)) for n in 0:(4^N -1)]
+    next(n) = [collect(2:n); 1]
+    params = Params(LATTICE_DIMS, [next(d) for d in LATTICE_DIMS])
 
-    U = 1.0
-    t = 1.0
-
-    nw = Dates.format(now(), "mm-dd HH:MM:SS")
+    t_start_calculations = now()
+    nw = Dates.format(t_start_calculations, "mm-dd HH:MM:SS")
     println("$nw  Begin calculations...")
 
-    thermodynamics = [[compute_thermodynamic_quantities(params, chem_pot, temperature, U, t)
-            for chem_pot in chem_potentials] for temperature in temp_range]
+    avg_n = Array{Array{Float64}}(undef, length(temp_range))
+    entropy_vals = Array{Array{Float64}}(undef, length(temp_range))
+    grand_pots = Array{Array{Float64}}(undef, length(temp_range))
 
-    nw = Dates.format(now(), "mm-dd HH:MM:SS")
+    for temp_idx in eachindex(temp_range)
+        temp_avg_n = Array{Float64}(undef, length(chem_potentials))
+        temp_entropy_vals = Array{Float64}(undef, length(chem_potentials))
+        temp_grand_pots = Array{Float64}(undef, length(chem_potentials))
+        Threads.@threads for chem_idx in eachindex(chem_potentials)
+            density, avg_particles, entropy, grand_pot = compute_thermodynamic_quantities(params, 
+                chem_potentials[chem_idx], temp_range[temp_idx], U, t, multiplier)
+            
+            temp_avg_n[chem_idx] = avg_particles
+            temp_entropy_vals[chem_idx] = entropy
+            temp_grand_pots[chem_idx] = grand_pot
+        end
+        avg_n[temp_idx] = temp_avg_n
+        entropy_vals[temp_idx] = temp_entropy_vals
+        grand_pots[temp_idx] = temp_grand_pots
+    end
+
+    t_end_calculations = now()
+    nw = Dates.format(t_end_calculations, "mm-dd HH:MM:SS")
     println("$nw  Calculations done.")
-
-    avg_n = [[thermodynamics[j][i][2] for i in eachindex(chem_potentials)] for j in eachindex(temp_range)]
-    entropy_vals = [[thermodynamics[j][i][3] for i in eachindex(chem_potentials)] for j in eachindex(temp_range)]
-    grand_pots = [[thermodynamics[j][i][4] for i in eachindex(chem_potentials)] for j in eachindex(temp_range)]
 
     helmholtz_vals = [[grand_pot + chem_pot*avg_no for (grand_pot, chem_pot, avg_no) in zip(grand_pot_rows, chem_potentials, avg_no_rows)] 
         for (grand_pot_rows, avg_no_rows) in zip(grand_pots, avg_n)]
@@ -112,14 +124,18 @@ function main()
 
     nw = Dates.format(now(), "mm-dd HH:MM:SS")
     println("$nw  Saving plots...")
-    save_graph(chem_potentials, avg_n, U, t, temp_range, "$STR_LATTICE_DIMS srednia liczba czastek")
-    save_graph(chem_potentials, entropy_vals, U, t, temp_range, "$STR_LATTICE_DIMS entropia")
-    save_graph(chem_potentials, grand_pots, U, t, temp_range, "$STR_LATTICE_DIMS wielki potencjal")
-    save_graph(chem_potentials, helmholtz_vals, U, t, temp_range, "$STR_LATTICE_DIMS energia sw helmholtza")
-    save_graph(chem_potentials, U_vals, U, t, temp_range, "$STR_LATTICE_DIMS energia wewnetrzna")
-    
-    nw = Dates.format(now(), "mm-dd HH:MM:SS")
-    println("$nw  Finished.")
+    mkpath("/$STR_LATTICE_DIMS")
+    save_graph(path, chem_potentials, avg_n, U, t, temp_range, "Srednia liczba czastek")
+    save_graph(path, chem_potentials, entropy_vals, U, t, temp_range, "Entropia")
+    save_graph(path, chem_potentials, grand_pots, U, t, temp_range, "Wielki potencjal")
+    save_graph(path, chem_potentials, helmholtz_vals, U, t, temp_range, "Energia sw helmholtza")
+    save_graph(path, chem_potentials, U_vals, U, t, temp_range, "Energia wewnetrzna")
+
+    t_end_program = now()
+    nw = Dates.format(t_end_program, "mm-dd HH:MM:SS")
+    total_calc_time = string(t_end_calculations - t_start_calculations)
+    total_exec_time = string(t_end_program - t_start_program)
+    println("$nw  Finished in $total_calc_time (total: $total_exec_time).")
 end
 
 main()
